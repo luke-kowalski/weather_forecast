a2c01e3e5795b9bffda247d3996638d9d30da119
LK <lukasz.fialki@gmail.com>
2021-08-17 08:22:49 +0200

    fix api url, add iterate over days

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ api = "3af3d3b40ec932ed51b8c87bfecc03d6"
 lat = 53.216198
 lon = 19.654487
 
-url = f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&{lon}=-94.04&exclude=hourly,daily&appid={api}'
+url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&exclude=hourly,minutely&appid={api}"
+
+
+
+response = requests.get(url)
+jsonResponse = response.json()
+
 
 print(url)
+print(jsonResponse['daily'])
+
+for days in jsonResponse['daily']:
+    print(days['dt'])
