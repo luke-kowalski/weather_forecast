503b07543e86569e7907b83268d157f4919a7512
LK <lukasz.fialki@gmail.com>
2021-08-18 09:00:02 +0200

    date formating

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 api = "3af3d3b40ec932ed51b8c87bfecc03d6"
 lat = 53.216198
@@ -7,13 +8,17 @@ lon = 19.654487
 url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&exclude=hourly,minutely&appid={api}"
 
 
-
 response = requests.get(url)
 jsonResponse = response.json()
 
 
-print(url)
-print(jsonResponse['daily'])
-
-for days in jsonResponse['daily']:
-    print(days['dt'])
+for days in jsonResponse["daily"]:
+    print(datetime.fromtimestamp(int(days["dt"])).strftime("%Y,%m,%d"))
+    print(days["dt"])
+    print(datetime.fromtimestamp(int(days["sunrise"])).strftime("%H:%M:%S"))
+    print(datetime.fromtimestamp(int(days["sunset"])).strftime("%H:%M:%S"))
+    print(days["temp"]["day"])
+    print(days["temp"]["night"])
+    print(days["pressure"])
+    print(days["humidity"])
+    print((days["weather"][0]["main"] + " - " + days["weather"][0]["description"]))
